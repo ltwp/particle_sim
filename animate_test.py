02b8faad826585ebfaf0769122cfb16f63b0df9f
Lukas <lukaswinklerprins@gmail.com>
2016-11-27 11:30:00 -0800

    split animation test

--- animate_test.py
+++ animate_test.py
@@ -23,10 +23,35 @@ xpp = numpy.ones((n_particles,2))
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1) # one row, one column, first plot
 
+def init():
+    global x
+    ax.scatter(x[:,0],x[:,1])
+    return ax,
+
+def animate(i):
+    canvas.restore_region(background)
+    global x
+    global xp
+    global xpp
+    global step_size
+    x += xp*step_size
+    xp += xpp*step_size
+    ax.scatter(x[:,0],x[:,1])
+    return ax,
+
+
+"""
 for i in range(1,n_steps):
     x += xp*step_size
     xp += xpp*step_size
     print(x)
     ax.scatter(x[:,0],x[:,1])
+"""
 
+# ----------
+# Do It
+
+# anim = animation.FuncAnimation(fig,animate,init_func=init,frames=n_steps,interval=20,blit=True)
+
+anim = animation.FuncAnimation(fig,animate,init_func=init,frames=n_steps,interval=20,blit=True)
 plt.show()
